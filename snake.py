a143b0afc9e9ab7e90260284be99c5d6c607e176
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-05-29 11:18:05 +0200

    feat: detect collisions with the snake tail

--- snake.py
+++ snake.py
@@ -6,36 +6,44 @@ MOVE_DISTANCE = 20
 class Snake:
   
   def __init__(self):
-    self.snake_segments = []
+    self.segments = []
     self.create_snake()
-    self.head = self.snake_segments[0]
+    self.head = self.segments[0]
 
   def create_snake(self):
     """Creates the snake body"""
     for position in SNAKE_STARTING_POSITIONS:
-      snake_segment = Turtle(shape="square")
-      snake_segment.color("white")
-      snake_segment.penup()
-      snake_segment.goto(position)
-      self.snake_segments.append(snake_segment)
+      self.add_segment(position)
+
+  def add_segment(self, position):
+    snake_segment = Turtle(shape="square")
+    snake_segment.color("white")
+    snake_segment.penup()
+    snake_segment.goto(position)
+    self.segments.append(snake_segment)
+
+  def extend(self):
+    """Adds a new segment to the snake"""
+    # -1 in the segments means that starts counting in the end of the list
+    self.add_segment(self.segments[-1].position())
   
   def move(self):
     """Moves each snake's segment position to the position of the preceding segment position (3 to 2, 2 to 1, etc.)"""    
     # range(start, stop, step)
-    for seg_num in range(len(self.snake_segments) - 1, 0, -1):
-      new_x_position = self.snake_segments[seg_num - 1].xcor()
-      new_y_position = self.snake_segments[seg_num - 1].ycor()
-      self.snake_segments[seg_num].goto(new_x_position, new_y_position)
+    for seg_num in range(len(self.segments) - 1, 0, -1):
+      new_x_position = self.segments[seg_num - 1].xcor()
+      new_y_position = self.segments[seg_num - 1].ycor()
+      self.segments[seg_num].goto(new_x_position, new_y_position)
 
     # moving first snake's segment 20 spaces
-    self.snake_segments[0].forward(MOVE_DISTANCE)
+    self.segments[0].forward(MOVE_DISTANCE)
 
 
   def left(self):
     """Moves the snake to the left"""
-    self.snake_segments[0].left(90)
+    self.segments[0].left(90)
 
   def right(self):
     """Moves the snake to the righ"""
-    self.snake_segments[0].right(90)
+    self.segments[0].right(90)
   
