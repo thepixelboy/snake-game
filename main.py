840cdb489d3ed20f3c24ffd2abc582dc50a88619
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-05-29 11:41:59 +0200

    refactor: detect the collision with the tail slicing the snake.segments list

--- main.py
+++ main.py
@@ -42,10 +42,10 @@ while game_is_on:
     scoreboard.game_over()
 
   # detect collision with tail
-  for segment in snake.segments:
-    if segment == snake.head:
-      pass
-    elif snake.head.distance(segment) < 10:
+  # slicing the snake.segment list on the for loop to start
+  # iterating at the position one of the list
+  for segment in snake.segments[1:]:
+    if snake.head.distance(segment) < 10:
       game_is_on = False
       scoreboard.game_over()
 
